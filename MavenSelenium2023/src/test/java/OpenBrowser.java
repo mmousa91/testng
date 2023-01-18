a0233a78487859a17d6c81024483a699a0510718
mmousa91 <109099345+mmousa91@users.noreply.github.com>
2023-01-18 15:43:49 +0200

    p

--- MavenSelenium2023/src/test/java/OpenBrowser.java
+++ MavenSelenium2023/src/test/java/OpenBrowser.java
@@ -1,13 +1,32 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenBrowser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        String ChromePath = System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", ChromePath);
+
+        //     System.setProperty("webdriver.chrome.driver", "C:\\Users\\MMousa\\IntelliJIDEAProjects\\MavenSelenium2023\\src\\main\\resources\\chromedriver.exe");
         WebDriver driver  = new ChromeDriver() ;
 
-        driver.get("https://www.google.com/");
 
+        //xpath with parent
+        driver.findElement(By.xpath("//form[@id=\"login\"]//button[@type=\"submit\"]")).click();
+
+// css selector with parent
+        //  driver.findElement(By.cssSelector("form > button[type=\"submit\"]")).click();
+
+        //css selector
+        // driver.findElement(By.cssSelector("button[type=\"submit\"]"));
+
+        // driver.findElement(By.linkText("Elemental Selenium")).click();
+        // driver.findElement(By.partialLinkText("Selenium")).click();
+        Thread.sleep(3000);
+
+        //driver.close();
+        driver.quit();
 
 
     }
