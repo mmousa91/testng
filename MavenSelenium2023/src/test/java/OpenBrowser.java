21ff28e1b9ce39db934a2c3eae5a8b72ca2c514d
mmousa91 <109099345+mmousa91@users.noreply.github.com>
2023-01-18 20:10:39 +0200

    testng-v1

--- MavenSelenium2023/src/test/java/OpenBrowser.java
+++ MavenSelenium2023/src/test/java/OpenBrowser.java
@@ -2,48 +2,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class OpenBrowser {
-    public static void main(String[] args) throws InterruptedException {
+
+    WebDriver driver  = null ;
+
+
+    @BeforeTest
+    public void OpenBrowser(){
 
         String ChromePath = System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", ChromePath);
+         driver  = new ChromeDriver() ;
 
-        //     System.setProperty("webdriver.chrome.driver", "C:\\Users\\MMousa\\IntelliJIDEAProjects\\MavenSelenium2023\\src\\main\\resources\\chromedriver.exe");
-        WebDriver driver  = new ChromeDriver() ;
 
         driver.navigate().to("https://the-internet.herokuapp.com/login");
         driver.manage().window().maximize();
+    }
 
+    @Test
+    public void ValidData (){
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
 
-        //get message
-        Thread.sleep(2000);
-        String Flashmsg = driver.findElement(By.id("flash")).getText();
-        System.out.println(Flashmsg);
-
-        //get css color
-
-        String BackColor=driver.findElement(By.id("flash")).getCssValue("background-color");
-        System.out.println(BackColor);
+    }
+    @Test
+    public void InvalidData (){
 
 
-        //xpath with parent
-      //  driver.findElement(By.xpath("//form[@id=\"login\"]//button[@type=\"submit\"]")).click();
+    }
+    @AfterTest
+    public void CloseBrowser(){
 
-// css selector with parent
-        //  driver.findElement(By.cssSelector("form > button[type=\"submit\"]")).click();
-
-        //css selector
-        // driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-
-        // driver.findElement(By.linkText("Elemental Selenium")).click();
-        // driver.findElement(By.partialLinkText("Selenium")).click();
-        Thread.sleep(3000);
-
-        //driver.close();
         driver.quit();
 
 
